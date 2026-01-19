4d0a6165ecca4dc2d3e2f3d50264fe2b95a0a9ec
Sarathi Thirumalai Soundararajan <tssarathi5901@gmail.com>
2026-01-19 15:46:15 +1100

    chore(infra): add DDL for airline loyalty raw table

--- src/config.py
+++ src/config.py
@@ -5,4 +5,4 @@ load_dotenv()
 
 AWS_REGION = os.getenv("AWS_REGION", "ap-southeast-4")
 S3_BUCKET = os.getenv("S3_BUCKET", "airline-customer-analytics")
-GLUE_DATABASE = os.getenv("GLUE_DB", "airline_analytics")
+GLUE_DATABASE = os.getenv("GLUE_DB", "airline-analytics")

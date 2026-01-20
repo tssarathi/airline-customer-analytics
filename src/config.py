5b76e284c3ef468cfa3e24a6a5fd133a2eff1340
Sarathi Thirumalai Soundararajan <tssarathi5901@gmail.com>
2026-01-20 13:44:44 +1100

    chore(config): fixed config repo placement

--- src/config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-AWS_REGION = os.getenv("AWS_REGION", "ap-southeast-4")
-S3_BUCKET = os.getenv("S3_BUCKET", "airline-customer-analytics")
-GLUE_DATABASE = os.getenv("GLUE_DB", "airline-analytics")
---
+++ src/config/config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+AWS_REGION = os.getenv("AWS_REGION")
+S3_BUCKET = os.getenv("S3_BUCKET")
+RAW_FOLDER = os.getenv("RAW_PREFIX")
+PROCESSED_FOLDER = os.getenv("PROCESSED_PREFIX")
+GLUE_DATABASE = os.getenv("GLUE_DB")
+ATHENA_RESULTS_FOLDER = os.getenv("ATHENA_RESULTS_PREFIX")

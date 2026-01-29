66b48d03b9c777050a0e5d052d9ede225da4e1a6
Sarathi Thirumalai Soundararajan <tssarathi5901@gmail.com>
2026-01-29 13:29:39 +1100

    feat(feature-exploration): created customer features table locally

---
+++ infra/sql/01_create_customer_features_table.sql
@@ -0,0 +1,21 @@
+CREATE EXTERNAL TABLE IF NOT EXISTS airline_analytics.customer_features (
+    loyalty_number BIGINT,
+    country STRING,
+    province STRING,
+    city STRING,
+    gender STRING,
+    education STRING,
+    loyalty_card STRING,
+    clv DOUBLE,
+    is_cancelled BOOLEAN,
+    tenure_months INT,
+    recency INT,
+    r_score INT,
+    frequency INT,
+    f_score INT,
+    monetary BIGINT,
+    m_score INT,
+    rfm_segment STRING
+)
+STORED AS PARQUET
+LOCATION 's3://airline-customer-analytics/curated/customer_features/';
